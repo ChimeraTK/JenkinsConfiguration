f2455bd55d07280b27e3d1506a9c912e7c2ebfc9
Martin Hierholzer <martin.hierholzer@desy.de>
2018-11-08 09:44:51 +0100

    added script to build DOOCS libraries

---
+++ vars/buildDoocsLibrary.groovy
@@ -0,0 +1,76 @@
+/***********************************************************************************************************************
+
+  buildDoocsLibrary() is called from the .jenkinsfile of each project
+
+***********************************************************************************************************************/
+
+// This is the function called from the .jenkinsfile
+// libraryName is the name of the library incl. the last part of the path in the repository, e.g. "common/clientlib"
+def call(String libraryName, ArrayList<String> dependencyList) {
+
+  // List of builds to be run. Format must be "<docker_image_name>-<cmake_build_type>"
+  def builds = [ 'xenial-Debug',
+                 'xenial-Release',
+                 'bionic-Debug',
+                 'bionic-Release',
+                 'tumbleweed-Debug',
+                 'tumbleweed-Release' ]
+
+  pipeline {
+    agent none
+    stages {
+      stage('build') {
+        // Run the build stages for all labels + build types in parallel, each in a separate docker container
+        steps {
+          script {
+            parallel builds.collectEntries { ["${it}" : transformIntoStep(libraryName, dependencyList, it)] }
+          }
+        }
+      } // end stage build
+    } // end stages
+  } // end pipeline
+
+}
+
+/**********************************************************************************************************************/
+
+def transformIntoStep(String libraryName, ArrayList<String> dependencyList, String buildName) {
+  // split the build name at the '-'
+  def (label, buildType) = buildName.tokenize('-')
+  // we need to return a closure here, which is then passed to parallel() for execution
+  return {
+    stage(buildName) {
+      node('Docker') {
+        // we need root access inside the container and access to the dummy pcie devices of the host
+        def dockerArgs = "-u 0 --device=/dev/mtcadummys0 --device=/dev/mtcadummys1 --device=/dev/mtcadummys2 --device=/dev/mtcadummys3 --device=/dev/llrfdummys4 --device=/dev/noioctldummys5 --device=/dev/pcieunidummys6 -v /var/run/lock/mtcadummy:/var/run/lock/mtcadummy"
+        docker.image("builder:${label}").inside(dockerArgs) {
+          script {
+            sh '''
+              mkdir /scratch
+              mkdir -p /export/doocs/library/common
+            '''
+            helper.doDependencyArtefacts(dependencyList, label, buildType)
+
+            sh """
+              mkdir -p /export/doocs/library/${libraryName}
+              cd /export/doocs/library/${libraryName}
+              chown -R msk_jenkins /export/doocs
+              sudo -E -u msk_jenkins git clone http://doocs-git.desy.de/cgit/doocs/library/${libraryName}.git .
+              export DOOCSARCH=Ubuntu-16.04-x86_64
+              make -j8
+              make install
+            """}
+            sh """
+              sudo -u msk_jenkins tar zcf install-${JOB_NAME}-${label}-${buildType}.tgz /export
+            """
+            archiveArtifacts artifacts: "install-${JOB_NAME}-${label}-${buildType}.tgz", onlyIfSuccessful: false
+          }
+        }
+      }
+    }
+  }
+}
+
+/**********************************************************************************************************************/
+
+
