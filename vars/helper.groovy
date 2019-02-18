825bf91e98a9d880a72f47740fd508250c4b0e34
Martin Hierholzer <martin.hierholzer@desy.de>
2019-02-18 13:13:48 +0100

    added missing escape character

--- vars/helper.groovy
+++ vars/helper.groovy
@@ -167,7 +167,7 @@ def doBuild(String label, String buildType) {
       SUBDIR="${env.RUN_FROM_SUBDIR}"
      fi
      for VAR in \${JOB_VARIABLES}; do
-       export ${VAR}
+       export \${VAR}
      done
      sudo -H -u msk_jenkins cmake /scratch/source/\${SUBDIR} -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=${buildType} -DSUPPRESS_AUTO_DOC_BUILD=true \${CMAKE_EXTRA_ARGS}
       sudo -H -u msk_jenkins make ${env.MAKEOPTS}
@@ -195,7 +195,7 @@ def doTest(String label, String buildType) {
       CTESTOPTS="${env.MAKEOPTS}"
     fi
     for VAR in \${JOB_VARIABLES}  \${TEST_VARIABLES}; do
-       export ${VAR}
+       export \${VAR}
     done
     sudo -H -u msk_jenkins ctest --no-compress-output \${CTESTOPTS} -T Test -V || true
     sudo -H -u msk_jenkins sed -i Testing/*/Test.xml -e 's_\\(^[[:space:]]*<Name>\\)\\(.*\\)\\(</Name>\\)\$_\\1${label}.${buildType}.\\2\\3_'
