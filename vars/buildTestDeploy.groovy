30c61e398239f6538227b61ff71f17ba7f8fc2b4
Martin Hierholzer <martin.hierholzer@desy.de>
2020-04-27 10:44:01 +0200

    Update buildTestDeploy.groovy

--- vars/buildTestDeploy.groovy
+++ vars/buildTestDeploy.groovy
@@ -98,7 +98,10 @@ def call(ArrayList<String> dependencyList, String gitUrl='',
                   git remote add project-template "https://github.com/ChimeraTK/project-template" || true
                   git remote set-url origin `echo ${gitUrl} | sed -e 's_http://doocs-git.desy.de/cgit/_git@doocs-git.desy.de:_' -e 's_/\$__'`
                   git remote update
-                  git merge -s theirs --squash --no-edit project-template/master && git commit -m "automatic merge of project-template" && git push --all || true
+                  git merge -X theirs --squash --no-edit project-template/master && \
+                  git commit -m "automatic merge of project-template" && \
+                  git push --all || \
+                  true
                 """
                 // We could also apply the clang-format style here, but this should be discussed first.
                 //  find \( -name '*.cc' -o -name '*.cxx' -o -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' -o -name '*.hxx' -o -name '*.hh' \) -exec clang-format-6.0 -style=file -i \{\} \;
