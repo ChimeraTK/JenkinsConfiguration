866281f4cd30c4439baa37bfccc312d425638ff2
Martin Killenberg <martin.killenberg@desy.de>
2021-03-08 15:49:21 +0100

    added step to modify image after creation to work aroud bug when creating images.

---
+++ dockerfiles/tumbleweed.modify_image.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# Unfortunately some of the package installations on tumbleweed require the
+# --security-opt seccomp:unconfined flag, which is not available when building an image. So here is the work-around:
+
+# Create a container with the required flag and start it
+docker create -i --security-opt seccomp:unconfined --name tumbleweed_updater tumbleweed
+docker start tumbleweed_updater
+
+# Execute the required commands in the container
+docker exec tumbleweed_updater zypper -n install git libboost_*-devel libqt5-qtbase-devel rpcbind
+docker exec tumbleweed_updater useradd -u 30996 msk_jenkins
+docker exec tumbleweed_updater echo "Defaults set_home" >> /etc/sudoers
+docker exec tumbleweed_updater git config --system http.proxy http://xfelproxy.desy.de:3128 
+docker exec tumbleweed_updater git config --system https.proxy http://xfelproxy.desy.de:3128
+
+# Stop the container and commit the changes to the image
+docker stop tumbleweed_updater
+docker commit tumbleweed_updater tumbleweed
+
+# Finally remove the container
+docker container rm tumbleweed_updater
