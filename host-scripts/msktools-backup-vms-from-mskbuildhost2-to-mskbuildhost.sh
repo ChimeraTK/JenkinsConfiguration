844817f53f41a8833e1c8244606df81cdea95bc8
Martin Hierholzer <martin.hierholzer@desy.de>
2024-04-18 15:01:35 +0200

    backup mskbuildhost2 itself as well

--- host-scripts/msktools-backup-vms-from-mskbuildhost2-to-mskbuildhost.sh
+++ host-scripts/msktools-backup-vms-from-mskbuildhost2-to-mskbuildhost.sh
@@ -10,3 +10,7 @@ RSYNC_ARGS='-ax --exclude=swap.img --delete --delete-before'
 for vm in ${VMS_TO_BACKUP}; do
   rsync ${RSYNC_ARGS} "${vm}:/" "${BACKUP_TARGET}/${vm}/"
 done
+
+
+rsync ${RSYNC_ARGS} "mskbuildhost2:/" "${BACKUP_TARGET}/mskbuildhost2-root/"
+rsync ${RSYNC_ARGS} "mskbuildhost2:/var" "${BACKUP_TARGET}/mskbuildhost2-var/"
