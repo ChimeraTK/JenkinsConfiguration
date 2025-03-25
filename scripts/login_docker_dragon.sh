b10c0608f9c13bce4c59da837c4206636d5cb8a3
Martin Hierholzer <martin.hierholzer@desy.de>
2025-03-25 10:06:37 +0100

    feat(login-docker): disable ip6net

--- scripts/login_docker_dragon.sh
+++ scripts/login_docker_dragon.sh
@@ -15,7 +15,7 @@ projectName=$3
 ARTEFACTS="/home/msk_jenkins/dragon-artefacts"
 
 # should be the same as in the pipeline script (excluding the -u 0)
-DOCKER_PARAMS="--privileged --shm-size=1GB --device=/dev/mtcadummys0 --device=/dev/mtcadummys1 --device=/dev/mtcadummys2 --device=/dev/mtcadummys3 --device=/dev/llrfdummys4 --device=/dev/noioctldummys5 --device=/dev/pcieunidummys6 --device=/dev/ctkuiodummy -v /var/run/lock/mtcadummy:/var/run/lock/mtcadummy -v /opt/matlab_R2016b:/opt/matlab_R2016b"
+DOCKER_PARAMS="--privileged --shm-size=1GB --device=/dev/mtcadummys0 --device=/dev/mtcadummys1 --device=/dev/mtcadummys2 --device=/dev/mtcadummys3 --device=/dev/llrfdummys4 --device=/dev/noioctldummys5 --device=/dev/pcieunidummys6 --device=/dev/ctkuiodummy -v /var/run/lock/mtcadummy:/var/run/lock/mtcadummy -v /opt/matlab_R2016b:/opt/matlab_R2016b --network ip6net"
 
 # create and start container (use options to allow gdb inside!)
 ID=`docker create --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -i ${DOCKER_PARAMS} -v /home/msk_jenkins:/home/msk_jenkins builder:${label}`
